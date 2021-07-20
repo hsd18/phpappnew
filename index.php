d745676d90c8f21347afa8723a5266d05ebe3e98
ganjivasu <53328237+ganjivasu@users.noreply.github.com>
2021-07-20 18:05:22 +0530

    Update index.php

--- index.php
+++ index.php
@@ -51,8 +51,6 @@
                 <a href="#about" class="btn btn-secondary">About</a>
                 <a href="#contact" class="btn btn-success">Contact</a>
                 <a class="btn btn-warning" href="#signup">Sign Up</a>
-                <a class="btn btn-warning" href="#signup">DevOps </a>    
-                 <a class="btn btn-warning" href="#signup">AWS </a>
             </div>
         </div>
     </nav>
